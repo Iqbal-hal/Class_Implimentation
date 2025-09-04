5d718721f64f7f8ca9ada8c5bdf497e0962f2c34
Iqbal-hal <iqbal.hal@gmail.com>
2025-09-04 23:32:58 +0530

    Update scrip_extractor comment to reference TradingWorkbench

--- support_files/scrip_extractor.py
+++ support_files/scrip_extractor.py
@@ -1,4 +1,9 @@
-﻿def scrip_extractor(master_df):
+﻿# (No functional changes expected here)
+# If this file imports via the package root, update it:
+# from TradingWorkbench.support_files.File_IO import something
+
+
+def scrip_extractor(master_df):
     '''
     master_df contains multiple scrips which is concateneated already.The function extract each scrips dataframe from the master_df.
 
